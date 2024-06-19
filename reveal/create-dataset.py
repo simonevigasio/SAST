55efd16a481806913fc184cbf508aef2b7cce9ce
Simone Vigasio <svigasio@fbk.eu>
2024-06-19 17:04:03 +0200

    set reveal in four files

--- reveal/create-dataset.py
+++ reveal/create-dataset.py
@@ -3,9 +3,17 @@ import pandas as pd
 
 db = pd.read_pickle('reveal_test.pkl')
 
+quarter_dblen = len(db['functionSource']) / 4
+
 i = 0
 for func in db['functionSource']: 
-    f = open('./data/'+str(i)+'.cpp', 'w')
+    j = 1
+    if quarter_dblen < i: 
+        if (2 * quarter_dblen) > i: j = 2
+        elif (3 * quarter_dblen) > i: j = 3
+        else: j = 4
+
+    f = open('./data'+str(j)+'/'+str(i)+'.cpp', 'w')
     f.write(func)
     f.close()
     i += 1
