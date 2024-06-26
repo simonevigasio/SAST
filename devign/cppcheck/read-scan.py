3143955f736e566eed7a78b2de04c20d2c989fa6
Simone Vigasio <svigasio@fbk.eu>
2024-06-26 16:01:52 +0200

    cppcheck per devign fatto

--- devign/cppcheck/read-scan.py
+++ devign/cppcheck/read-scan.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 idx = set()
 with open('../assets/test.txt') as f:
@@ -10,23 +11,120 @@ data = pd.read_json('../assets/Devign.json')
 db = data.iloc[list(idx)]
 out = open('output.txt', 'r')
 
-errs = []
+rule_types = []
+files = []
+
 for line in out:    
 
     if line[:6].startswith('devign'):
 
+        file = { 'name': '', 'rules': [] }
+
+        i = 0
+        while (line[i] != '.' or line[i+1] != 'c'):
+            file['name'] += line[i]
+            i += 1
+
+        i += 3
+        while line[i] != ':':
+            i += 1
+
+        i += 1
+        while line[i] != ':':
+            i += 1
+
+        rule_category = ''
+        i += 2
+        while line[i] != ':':
+            rule_category += line[i]
+            i += 1
+
         line = line[::-1]
-        err = ''
+        rule_type = ''
 
-        counter = 1
-        if line[counter] == ']':
-            while line[counter] != '[':
-                err += line[counter]
-                counter += 1
+        if line[1] == ']':
+            i = 2
+            while line[i] != '[':
+                rule_type += line[i]
+                i += 1
                 
-        if err not in errs:
-            errs.append(err)
+        rule_type = rule_type[::-1]
 
-rules = ['arrayIndexOutOfBounds', 'shiftNegativeLHS', 'invalidPrintfArgType_sint', 'uninitvar', 'preprocessorErrorDirective', 'noValidConfiguration', 'arithOperationsOnVoidPointer', 'internalAstError', 'AssignmentIntegerToAddress', 'unknownEvaluationOrder', 'shiftTooManyBitsSigned', 'autoVariables', 'invalidPointerCast']
+        rule = { 'rule_category': rule_category, 'rule_type': rule_type }
 
-print(errs)
+        if (file['name'] != '') and (file['name'] not in [f['name'] for f in files]):
+            files.append(file)
+
+        if (rule_type != '') and (rule_type not in rule_types):
+            rule_types.append(rule_type)
+
+        if (rule_type != '') and (rule not in [f['rules'] for f in files if f['name'] == file['name']][0]): 
+            [f['rules'] for f in files if f['name'] == file['name']][0].append(rule)
+
+out.close()
+rule_types.sort()
+
+with open('stats.json', 'w') as f:
+    json.dump(files, f)
+
+idx_files = []
+valid_categories = ['error', 'portability', 'warning', 'style']
+not_valid_types = ['unusedFunction', 'syntaxError']
+
+for file in files: 
+
+    is_valid = False
+    for rule in file['rules']:
+        if (rule['rule_category'] in valid_categories) and (rule['rule_type'] not in not_valid_types):
+            is_valid = True
+
+    if is_valid:
+
+        file['name'] = file['name'][::-1]
+        idx_file = ''
+        
+        i = 0
+        while file['name'][i] != '/':
+            idx_file += file['name'][i]
+            i += 1
+
+        idx_file = int(idx_file[::-1])
+        idx_files.append(idx_file)
+
+idx_files.sort()
+results = [0]*len(db)
+
+for idx in idx_files:
+    results[idx] = 1
+
+preds = open('preds.txt', 'w')
+for result in results:
+    preds.write(str(result)+'\n')
+
+preds.close()
+
+TP, TN, FP, FN, c = 0, 0, 0, 0, 0
+for label in db.target:
+    pred = results[c]
+    c += 1
+
+    if pred == label: 
+        if pred == 0: TN += 1
+        else: TP += 1
+    else: 
+        if pred == 0: FN += 1
+        else: FP += 1
+
+Accuracy = (TP + TN) / (TN + TP + FP + FN)
+Precision = TP / (TP + FP)
+Recall = TP / (TP + FN)
+F1 = TP / (TP + ((FN + FP) / 2))
+
+print('TP', TP)
+print('TN', TN)
+print('FP', FP)
+print('FN', FN)
+print('Accuracy', Accuracy)
+print('Precision', Precision)
+print('Recall', Recall)
+print('F1', F1)
